720ca7a45498f38aad4345864ebd1ae62d03a851
Rares Chelariu <rareschelariu79@gmail.com>
2021-03-23 17:20:02 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace cntema3
+{
+    internal class Program
+    {
+        struct SparseElement
+        {
+            public int lin, col;
+            public double val;
+
+            public static SparseElement operator +(SparseElement a, SparseElement b)
+            {
+                SparseElement res = new SparseElement();
+                return res;
+            }
+        }
+
+        class SparseMatrix
+        {
+            private List<SparseElement> _matrix = new List<SparseElement>();
+            
+        }
+        public static void Main(string[] args)
+        {
+            
+        }
+        
+    }
+}
